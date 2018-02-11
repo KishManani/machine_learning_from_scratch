4f724ff5ed923e11730f6ee61918a109f81e7bc8
KishManani <kishan_manani@hotmail.com>
2018-02-11 17:47:38 +0000

    Implemented OLS and gradient descent for linear regression

--- machine_learning_from_scratch/supervised/linear_regression.py
+++ machine_learning_from_scratch/supervised/linear_regression.py
@@ -8,7 +8,18 @@ class LinearRegression(object):
         self.training_loss = None  # Stores training loss after running gradient descent
 
     def predict(self, X):
+        """
+        Predict outputs from an array of inputs
 
+        Parameters
+        ----------
+        X : Input data
+
+        Returns
+        -------
+        Array of predictions from trained linear regression model
+
+        """
         X = np.insert(X, 0, values=1, axis=1)  # Add constant 1 to start of array
 
         assert X.shape[1] == len(self.weights), "Input size of {0} does not match expected size of {1}".format(
@@ -16,18 +27,54 @@ class LinearRegression(object):
         return np.dot(X, self.weights)
 
     def fit(self, X, y, method='OLS', epochs=None, learning_rate=None, batch_size=None):
+        """
+        Fit linear regression model given training data and target array
+
+        Parameters
+        ----------
+        X : Training data
+        y : Target values
+        method : 'OLS' for ordinary least squares solution, 'SGD' for batch gradient descent
+        epochs : Number of epochs to run gradient descent
+        learning_rate : Learning rate for gradient descent
+        batch_size : Batch size for batch gradient descent
+
+        Returns
+        -------
+        None
+
+        """
+
+        # Add a column of ones to training data
+        X = np.insert(X, 0, values=1, axis=1)
 
         if method == 'OLS':
-            X = np.insert(X, 0, values=1, axis=1)  # Add a column of ones to the start
-            ols = np.dot(np.linalg.inv(np.dot(X.T, X)), np.dot(X.T, y))
-            self.weights = ols
+            ols_weights = np.dot(np.linalg.inv(np.dot(X.T, X)), np.dot(X.T, y))
+            self.weights = ols_weights
         elif method == 'SGD':
-            X = np.insert(X, 0, values=1, axis=1)  # Add a column of ones to the start
             self.gradient_descent(X, y, epochs=epochs, learning_rate=learning_rate, batch_size=batch_size)
+        else:
+            raise ValueError('Method "{0}" was not recognised. Method must be either "OLS" or "SGD".'.format(method))
+
         return None
 
     def gradient_descent(self, X, y, epochs, learning_rate, batch_size):
+        """
+        Optimises weights using gradient descent
 
+        Parameters
+        ----------
+        X : Training data
+        y : Target values
+        epochs : Number of epochs to run gradient descent
+        learning_rate : Learning rate for gradient descent
+        batch_size : Batch size for batch gradient descent
+
+        Returns
+        -------
+        None
+
+        """
         num_feats = X.shape[1]
         num_samples = X.shape[0]
 
@@ -58,7 +105,21 @@ class LinearRegression(object):
 
     @staticmethod
     def gradient_descent_step(W, X, y, learning_rate):
+        """
+        Performs a single step of gradient descent
 
+        Parameters
+        ----------
+        W : Weights
+        X : Training data
+        y : Target values
+        learning_rate : Learning rate for gradient descent
+
+        Returns
+        -------
+        W : Updated weights
+
+        """
         y_pred = np.dot(X, W)
         err = (y - y_pred)
         dW = np.dot(-X.T, err) / len(X)
